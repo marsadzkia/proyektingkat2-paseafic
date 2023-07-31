20510f1cbbbf29de8cca3c2d89874c042784deab
Marsa <marsaadz06@gmail.com>
2023-07-31 16:08:29 +0700

    update project

--- app/src/main/java/org/d3if2125/paseafic_nav/MainActivity.kt
+++ app/src/main/java/org/d3if2125/paseafic_nav/MainActivity.kt
@@ -3,7 +3,6 @@ package org.d3if2125.paseafic_nav
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import kotlinx.android.synthetic.main.activity_main.*
 import org.d3if2125.paseafic_nav.ui.belajar.BelajarFragment
 import org.d3if2125.paseafic_nav.ui.home.HomeFragment
 import org.d3if2125.paseafic_nav.ui.setting.SettingFragment
@@ -18,15 +17,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         moveFragment(homeFragment)
-
-        bottom_Nav.setOnNavigationItemReselectedListener {
-            when(it.itemId){
-                R.id.ic_home -> moveFragment(homeFragment)
-                R.id.ic_book -> moveFragment(belajarFragment)
-                R.id.ic_setting -> moveFragment(settingFragment)
-            }
-            true
-        }
     }
 
     private fun moveFragment(fragment: Fragment){
